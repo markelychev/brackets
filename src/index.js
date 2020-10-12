245396802f5219f138ebf4b226e6c9067b2c1c1e
A.Markelychev <a.markelychev@gmail.com>
2020-10-12 23:05:13 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let res = str;
+    if (res.length % 2) return false;
+    const confStr = bracketsConfig.map((arr) => arr.join(""));
+    const confRegExp = confStr.map(
+        (item) =>
+            new RegExp(
+                item.replace(
+                    /[\|,\(,\),\[,\],\{,\}]/g,
+                    (match) => "\\" + match
+                ),
+                "g"
+            )
+    );
+    for (let i = res.length / 2; i; i--) {
+        res = delBreckets(res, confRegExp);
+    }
+    return res.length ? false : true;
+
+    function delBreckets(str, regExpArr) {
+        let cleanStr = str;
+        regExpArr.forEach((element) => {
+            cleanStr = cleanStr.replace(element, "");
+        });
+        return cleanStr;
+    }
+};
